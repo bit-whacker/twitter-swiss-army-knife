699dad855c5f793b4723bbd407eeace87f1bf14a
uMklami <m.aamir.it@gmail.com>
2016-01-07 11:41:17 +0500

    account setting added

--- src/main/java/org/projectspinoza/twitterswissarmyknife/command/CommandDumpAccountSettings.java
+++ src/main/java/org/projectspinoza/twitterswissarmyknife/command/CommandDumpAccountSettings.java
@@ -5,23 +5,34 @@ import java.io.IOException;
 
 import org.projectspinoza.twitterswissarmyknife.util.TsakResponse;
 
-import com.beust.jcommander.Parameters;
-
+import twitter4j.AccountSettings;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
+
+import com.beust.jcommander.Parameters;
+import com.google.gson.Gson;
 
 @Parameters(commandNames = "dumpAccountSettings", commandDescription = "Account Setting")
 public class CommandDumpAccountSettings extends BaseCommand {
 
 	@Override
 	public TsakResponse execute(Twitter twitter) throws TwitterException {
-		// TODO Auto-generated method stub
-		return null;
+	    int remApiLimits = 0;
+	    AccountSettings settings = twitter.getAccountSettings();
+	    remApiLimits = settings.getRateLimitStatus().getRemaining();
+	    TsakResponse tsakResponse = new TsakResponse(remApiLimits, settings);
+        tsakResponse.setCommandDetails(this.toString());
+        return tsakResponse;
 	}
 
 	@Override
 	public void write(TsakResponse tsakResponse, FileWriter writer) throws IOException {
-		// TODO Auto-generated method stub
-		
+	    String jsonSettings = new Gson().toJson(tsakResponse);
+        writer.append(jsonSettings);
 	}
+
+    @Override
+    public String toString() {
+        return "CommandDumpAccountSettings []";
+    }
 }
